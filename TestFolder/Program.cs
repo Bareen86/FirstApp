1d1ba43084c6ddf4fdc0ecc9eb0d12006195f63e
Egor <79940875+Bareen86@users.noreply.github.com>
2023-03-04 15:57:34 +0300

    remove odd files

---
+++ TestFolder/Program.cs
@@ -0,0 +1,11 @@
+﻿using FirstApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("Egor");
+        person.Name = "Egor K";
+        Console.WriteLine(person.Name);
+    }
+}
