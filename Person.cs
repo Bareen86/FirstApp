e7b9cbf8e42d67b9b66fa06e8992bf3030e52a85
Egor <79940875+Bareen86@users.noreply.github.com>
2023-03-04 10:18:21 +0300

    first commit

---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    //  internal - виден внутри сборки
+    //  private - виден только внутри класса
+    internal class Person
+    {
+        private string _name;
+
+        public Person()
+        {
+
+        }
+
+        // Свойство с именем Get
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                // value - передаваемый параметр
+                _name = value;
+            }
+        }
+
+        public Person(string name)
+        {
+            _name = name;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+    }
+}
